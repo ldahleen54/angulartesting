3048ca70d4c15362e10477f5f016f0bd8d9c22c7
Luke Dahleen <ldahleen54@gmail.com>
2020-04-21 00:00:34 -0500

    Added click function and selected function

--- inventory-app/src/app/products-list/products-list.component.ts
+++ inventory-app/src/app/products-list/products-list.component.ts
@@ -2,17 +2,27 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from "../product.model";
 @Component({
   selector: 'products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  templateUrl: './products-list.component.html'
 })
 export class ProductsListComponent {
   @Input() productList: Product[];
-
   @Output() onProductSelected: EventEmitter<Product>;
   private currentProduct: Product;
 
   constructor() { 
     this.onProductSelected = new EventEmitter();
+  }
+
+  click(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  selected(product: Product): boolean {
+    if(!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
   }
 
 }
