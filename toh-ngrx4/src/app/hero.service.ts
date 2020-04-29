0f33c7762cbed61c36ae2374a20da76ca81a9bfc
Luke Dahleen <ldahleen54@gmail.com>
2020-04-28 22:44:17 -0500

    Message service gets used whenever a user clicks a hero. New message when heroes are retrieved. Added message view binded through message service

--- toh-ngrx4/src/app/hero.service.ts
+++ toh-ngrx4/src/app/hero.service.ts
@@ -3,16 +3,18 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { Hero } from './hero';
 import { HEROES, extractHeroes } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   // needs to be observable because async for server
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('Hero Service: fetched heroes');
     // of returns a single value for an observer
     return of(extractHeroes(0));
   }
