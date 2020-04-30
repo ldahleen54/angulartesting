84b67ffa566d7207b4b66523c228d682241b0cba
Luke Dahleen <ldahleen54@gmail.com>
2020-04-29 23:52:34 -0500

    added mock hero service and tested displaying heroes

--- toh-ngrx4/src/app/heroes/heroes.component.spec.ts
+++ toh-ngrx4/src/app/heroes/heroes.component.spec.ts
@@ -1,23 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Hero } from '../hero';
 import { HeroesComponent } from './heroes.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MessageService } from '../message.service';
+import { of } from 'rxjs/internal/observable/of';
+import { Observable } from 'rxjs/internal/Observable';
 
+class MockedHeroService {
+  private heroes = [{ id: 11, name: 'Glueman'}];
+  getHeroes(): Observable<Hero[]> {
+    return of(this.heroes);
+  }
+}
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
-
+  let debugElement: DebugElement;
+  let heroService: MockedHeroService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      declarations: [ HeroesComponent ],
+      providers: [ MockedHeroService, MessageService]
     })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    heroService = TestBed.inject(MockedHeroService);
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -31,8 +43,11 @@ describe('HeroesComponent', () => {
   it('Should retrieve heroes', async() => {
     expect(component.getHeroes()).toBeDefined();
   });
-  
-
-
+  it('should display heroes', async() => {
+    let text = debugElement
+      .query(By.css('li.hero'))
+      .nativeElement.innerText;
+    expect(text).toBeDefined();
+  }); 
 });
 
