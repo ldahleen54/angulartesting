f875593129dbfd1365f748c2e2e0c38839c46551
Luke Dahleen <ldahleen54@gmail.com>
2020-05-01 16:44:44 -0500

    fixed issue with hero details not displaying properly

--- toh-ngrx4/src/app/hero-detail/hero-detail.component.ts
+++ toh-ngrx4/src/app/hero-detail/hero-detail.component.ts
@@ -23,7 +23,7 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHeroes(id)
+    this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
