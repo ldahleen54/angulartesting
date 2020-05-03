5d99b2cdd2f2117e3878a151308241c49611af46
Luke Dahleen <ldahleen54@gmail.com>
2020-05-03 16:14:02 -0500

    Hero service makes a call to hero api for getting a hero by id or getting all heroes. Added error handling on that request.

--- toh-ngrx4/src/app/hero.service.ts
+++ toh-ngrx4/src/app/hero.service.ts
@@ -3,24 +3,51 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { Hero } from './hero';
 import { HEROES, extractHeroes } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesAPIUrl = 'api/heroes';
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('Hero Service: fetched heroes');
     // of returns a single value for an observer
-    return of(extractHeroes(0));
+    return this.http.get<Hero[]>(this.heroesAPIUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', [])),
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`Fetched hero: with id id=${id}`);
-    return of(extractHeroes(10).find(hero => hero.id === id));
+    const url = `${this.heroesAPIUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`Fetched hero with id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      )
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.log(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result);
+    }
   }
 
 }
