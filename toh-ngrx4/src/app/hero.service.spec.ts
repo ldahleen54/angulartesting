d29708f625dba3da3e7d44019c37f872ce3455e8
Luke Dahleen <ldahleen54@gmail.com>
2020-05-10 22:46:45 -0500

    Created failing test for getHero in heroservice

--- toh-ngrx4/src/app/hero.service.spec.ts
+++ toh-ngrx4/src/app/hero.service.spec.ts
@@ -1,17 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpEvent } from '@angular/common/http';
+import { Hero } from './hero';
 
 describe('HeroService', () => {
   let service: HeroService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(HeroService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should getHero', () => {
+    let hero: Hero;
+    service.getHero(12).subscribe((data) => {
+      hero = data;
+    });
+    expect(hero).toBeDefined();
   });
 
   // should services just be tested normally if they are retrieving data from a server
